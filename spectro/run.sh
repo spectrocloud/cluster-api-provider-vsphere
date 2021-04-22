698418006e44847b4fb59aa84bf6cc400c805668
LochanRn <rajanaga_ra@srmuniv.edu.in>
2021-04-22 14:32:20 +0530

    updated crds

--- spectro/run.sh
+++ spectro/run.sh
@@ -2,5 +2,5 @@
 
 rm generated/*
 
-kustomize build base > ./generated/core-base.yaml
-kustomize build global > ./generated/core-global.yaml
+./kustomize build base > ./generated/core-base.yaml
+./kustomize build global > ./generated/core-global.yaml
